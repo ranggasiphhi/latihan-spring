a7dbf5d8aaf19d9a67c54956d4deafcb79327959
rangga.s <ranggasahadewa.cakap@gmail.com>
2019-12-27 17:26:17 +0700

    still failed-but commit first

--- first-restapi/src/main/java/com/payroll/api/application/PayrollApplication.java
+++ first-restapi/src/main/java/com/payroll/api/application/PayrollApplication.java
@@ -1,9 +1,13 @@
 package com.payroll.api.application;
 
+import com.payroll.api.LoadDatabase;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.payroll.api"})
+@EnableJpaRepositories(basePackageClasses = LoadDatabase.class)
 public class PayrollApplication{
     public static void main(String... args){
         SpringApplication.run(PayrollApplication.class, args);
